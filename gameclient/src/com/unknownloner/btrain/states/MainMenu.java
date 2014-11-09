6a6b530695d3c94fd46ab7903c5eeadf6d7b5dbd
cobbleopolis <cobbleopolis@gmail.com>
2014-11-08 22:03:57 -0500

    Main Menu texture

--- gameclient/src/com/unknownloner/btrain/states/MainMenu.java
+++ gameclient/src/com/unknownloner/btrain/states/MainMenu.java
@@ -1,5 +1,6 @@
 package com.unknownloner.btrain.states;
 
+import com.unknownloner.btrain.gl.Texture;
 import org.lwjgl.opengl.Display;
 
 import java.io.IOException;
@@ -9,8 +10,11 @@ import static org.lwjgl.opengl.GL20.glUniformMatrix3;
 
 public class MainMenu extends GameState{
 
+    Texture texture;
+
     public MainMenu() throws IOException{
         super();
+        texture = Texture.load("/textures/space.jpg");
     }
 
 
@@ -31,8 +35,10 @@ public class MainMenu extends GameState{
         modelBuf.flip();
         glUniformMatrix3(shader.uniformLoc("u_model"), false, modelBuf);
 
+        batch.drawTexture(texture, 0, 0, Display.getWidth(), Display.getHeight());
+
         glUniform2f(shader.uniformLoc("u_screen_size"), Display.getWidth(), Display.getHeight());
-        batch.drawString("Hello World!", 50.0, 50.0, 3);
+        batch.drawString("Hello World!", 50.0, 50.0, 10);
 
         batch.end();
     }
