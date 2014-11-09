2ad0c763f07d2faf312f1b138cc7253deed1d9c1
cobbleopolis <cobbleopolis@gmail.com>
2014-11-09 01:14:52 -0500

    Bullet Texture

--- gameclient/src/com/unknownloner/btrain/states/LevelSelect.java
+++ gameclient/src/com/unknownloner/btrain/states/LevelSelect.java
@@ -1,6 +1,8 @@
 package com.unknownloner.btrain.states;
 
+import com.unknownloner.btrain.Util;
 import com.unknownloner.btrain.gl.Texture;
+import com.unknownloner.btrain.ui.MenuText;
 import org.lwjgl.opengl.Display;
 
 import java.io.IOException;
@@ -13,6 +15,21 @@ public class LevelSelect extends GameState {
     Texture texture;
 
     int select = 1;
+
+    String titleText = "Bullet Train";
+    int titleX = (Display.getWidth() - Util.stringWidth(titleText, 10)) / 2;
+    int titleY = Display.getHeight() * 3 / 4;
+    MenuText title = new MenuText(titleText, 10, titleX, titleY, 1.0f, 1.0f, 0.0f, 1.0f);
+
+    String levelText = "Level Select";
+    int levelX = (Display.getWidth() - Util.stringWidth(levelText, 5)) / 2;
+    int levelY = (Display.getHeight() * 11 / 20);
+    MenuText level = new MenuText(levelText, 5, levelX, levelY, 1.0f, 1.0f, 0.0f, 1.0f);
+
+    String exitText = "Exit Game";
+    int exitX = (Display.getWidth() - Util.stringWidth(exitText, 5)) / 2;
+    int exitY = (Display.getHeight() * 7 / 20);
+    MenuText exit = new MenuText(exitText, 5, exitX, exitY, 1.0f, 1.0f, 0.0f, 1.0f);
 
     public LevelSelect() throws IOException {
         texture = Texture.load("/textures/space.jpg");
